c6dc125ed58ddd9693fb77b311011a9e13db0e6c
ictaksupport3 <ictaksupport3@ictkerala.org>
2023-06-06 18:04:18 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+                    //print value
+// console.log('hello');
+                    //print warning
+//console.warn("warning");
+                    //print alert
+// alert('alert');
+                    // variabledeclaration
+// let s= null;
+                    //print the variable
+// console.log(s);
+// var d=0;
+                    //while
+// while(d<=10)
+// {
+//     console.log(d);
+//     d++;
+// }
+// let d=0;
+                //dowhile
+// do
+// {
+//     console.log(d+1);
+//     d++
+// }while(d<10);
+// let car={
+//     year :'2005',
+//     type :'i20',
+//     caemodel:'256'
+// }
+// //console.log(car);//{ year: '2005', type: 'i20', caemodel: '256' }
+// console.log(car.year);//2005
+
+                //array
+
+// const tody=[
+// {
+//     id:1,
+//     text:'man'
+// },
+// {
+//     id:2,
+//     text:'gg',
+     
+// }
+// ]
+// console.log(tody[0]);//{ id: 1, text: 'man' }
+// console.log(tody[0].text);//man
+// console.log(tody[1]);//{ id: 2, text: 'gg'     }
+// console.log(tody[1].text);//gg
+
+                    // fnuction
+// function add(a,b)
+// {
+//     let c=a+b;
+//     console.log(c);
+// }
+// add(10,22);//32
+
+                    //standerd funtion declaration
+// var add=(a,b)=>{
+//     let c=a+b;
+//     console.log(c);
+// }
+// add(10,22);//32
+
+// var sub=(a,b)=>{
+//     let c=a-b;
+//     console.log("the value",+c);     // the congatination
+//     console.log(`the value is ${c}`);   // the value print with the message
+//  }
+// sub(100,22);//78
+
+// let sub=()=>{
+//     console.log("kkk");
+// }
+// sub()//kkk
+                    //array spread operator
+// var a=[1,2,3];
+// var b=[4,5,6];
+// var c=[...a,...b];
+// console.log(c);//[ 1, 2, 3, 4, 5, 6 ]
+
+// var[a,b,...t]=[23,24,25,26,27];
+// console.log(t);//[ 25, 26, 27 ]
+// console.log(b);//24
+// console.log(a);//25
+
